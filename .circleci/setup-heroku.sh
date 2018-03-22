871ead67a8118cc4b40eb9dcc044d5128901c070
Roberto Oliveros <robertooliveros@mac.com>
2018-03-22 15:38:35 -0600

    Add new commands to sh script

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -8,9 +8,10 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
 
-cat >> ~/.ssh/config << EOF
-  VerifyHostKeyDNS yes
-  StrictHostKeyChecking no
-EOF
+# Add heroku.com to the list of known hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
